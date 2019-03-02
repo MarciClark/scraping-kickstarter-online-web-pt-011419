25b40b8a57c0b8647b4ad26e1f5f1779cb3fffdb
Marci Clark <marcig612@gmail.com>
2019-03-02 20:44:06 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -6,8 +6,8 @@ def create_project_hash
   kickstarter = Nokogiri::HTML(html)
  
   projects = {}
-  
-    # Iterate through the projects
+ 
+  # Iterate through the projects
   kickstarter.css("li.project.grid_4").each do |project|
     projects[project] = {}
   end
